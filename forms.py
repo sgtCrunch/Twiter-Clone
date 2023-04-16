5583766a611c8fc91d0411740ecaeafd74c7faf6
Brayan Pereira <brayan@codewithus.com>
2023-04-16 11:47:55 -0700

    Updated code to work with newer modules

--- forms.py
+++ forms.py
@@ -1,19 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import InputRequired, Email, Length
 
 
 class MessageForm(FlaskForm):
     """Form for adding/editing messages."""
 
-    text = TextAreaField('text', validators=[DataRequired()])
+    text = TextAreaField('text', validators=[InputRequired()])
 
 
 class UserAddForm(FlaskForm):
     """Form for adding users."""
 
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[InputRequired()])
+    email = StringField('E-mail', validators=[InputRequired(), Email()])
     password = PasswordField('Password', validators=[Length(min=6)])
     image_url = StringField('(Optional) Image URL')
 
@@ -21,5 +21,5 @@ class UserAddForm(FlaskForm):
 class LoginForm(FlaskForm):
     """Login form."""
 
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[Length(min=6)])
